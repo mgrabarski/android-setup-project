31825b48d519cadfc4b8e93334f5da6a154c012d
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2021-01-14 13:35:01 +0100

    Added dependencies

---
+++ buildSrc/src/main/kotlin/dependencies/AnnotationProcessorsDependencies.kt
@@ -0,0 +1,6 @@
+package dependencies
+
+object AnnotationProcessorsDependencies {
+    const val DATABINDING = "com.android.databinding:compiler:${BuildDependenciesVersions.DATABINDING}"
+    const val ROOM = "androidx.room:room-compiler:${BuildDependenciesVersions.ROOM}"
+}
